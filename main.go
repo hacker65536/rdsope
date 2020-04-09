8e009ec0cd3e1808a8904faca11410f9b46f6a19
hacker65536 <s.hacker65536@gmail.com>
2020-04-09 21:54:29 +0900

    pagnaitor

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
-import "os"
+import (
+	"os"
+)
 
 func main() {
 	//argsWithProg := os.Args
 	//argsWithoutProg := os.Args[1:]
-	getrds(os.Args[1])
+	if len(os.Args) == 1 {
+
+		getrds(``)
+	} else {
+		getrds(os.Args[1])
+
+	}
 
 }
--- rdsfunc.go
+++ rdsfunc.go
@@ -6,7 +6,6 @@ import (
 	"regexp"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/awserr"
 	"github.com/aws/aws-sdk-go-v2/service/rds"
 
 	//"github.com/fatih/color"
@@ -18,42 +17,39 @@ func getrds(filter string) {
 
 	params := &rds.DescribeDBInstancesInput{}
 	req := rdssvc.DescribeDBInstancesRequest(params)
-	resp, err := req.Send(context.TODO())
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case rds.ErrCodeDBInstanceNotFoundFault:
-				fmt.Println(rds.ErrCodeDBInstanceNotFoundFault, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
+
+	//resp, err := req.Send(context.TODO())
+	p := rds.NewDescribeDBInstancesPaginator(req)
+
+	for p.Next(context.TODO()) {
+		page := p.CurrentPage()
+
+		for _, v := range page.DBInstances {
+			if reg.MatchString(aws.StringValue(v.DBInstanceIdentifier)) {
+				//tags := make(map[string]string)
+				fmt.Println(color.Bold(aws.StringValue(v.DBInstanceIdentifier)))
+
+				/*
+					t := listTagsForResource(v.DBInstanceArn)
+						if len(t.ListTagsForResourceOutput.TagList) != 0 {
+							//fmt.Printf("[")
+							for _, v2 := range t.ListTagsForResourceOutput.TagList {
+								//tags[aws.StringValue(v2.Key)] = aws.StringValue(v2.Value)
+								fmt.Printf(`"` + aws.StringValue(v2.Key) + `"`)
+								fmt.Printf(`:`)
+								fmt.Printf(`"` + aws.StringValue(v2.Value) + `"`)
+								fmt.Println()
+							}
+							//fmt.Printf("]")
+						}
+						//fmt.Println(tags)
+				*/
 			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
 		}
-		return
 	}
 
-	for _, v := range resp.DBInstances {
-		if reg.MatchString(aws.StringValue(v.DBInstanceIdentifier)) {
-			//tags := make(map[string]string)
-			fmt.Println(color.Bold(aws.StringValue(v.DBInstanceIdentifier)))
-			t := listTagsForResource(v.DBInstanceArn)
-
-			if len(t.ListTagsForResourceOutput.TagList) != 0 {
-				//fmt.Printf("[")
-				for _, v2 := range t.ListTagsForResourceOutput.TagList {
-					//tags[aws.StringValue(v2.Key)] = aws.StringValue(v2.Value)
-					fmt.Printf(`"` + aws.StringValue(v2.Key) + `"`)
-					fmt.Printf(`:`)
-					fmt.Printf(`"` + aws.StringValue(v2.Value) + `"`)
-					fmt.Println()
-				}
-				//fmt.Printf("]")
-			}
-			//fmt.Println(tags)
-		}
+	if err := p.Err(); err != nil {
+		fmt.Println(err)
 	}
 
 }
